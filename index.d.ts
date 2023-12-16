1326778cb473d8b9591f678f68bfdd0667558cb7
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-12-16 12:58:23 +0100

    added NoCacheDB class

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 import { Collection } from '@discordjs/collection';
 import { Document, Model, Types } from 'mongoose';
 
-export = DB;
+export { DB, NoCacheDB }
+export default DB
+
 declare class DB {
   schema: Model<Document<unknown, {}, {
     _id: Types.ObjectId,
@@ -35,6 +37,42 @@ declare class DB {
   /**
    * @param key if not provided, the whole `db` gets deleted
    * @returns `true` if the element existed or the key param is provied and `false` if the element did not exist
+   */
+  delete(db: string, key?: string): Promise<boolean>;
+}
+
+declare class NoCacheDB {
+  schema: Model<Document<unknown, {}, {
+    _id: Types.ObjectId,
+    key: string,
+    value?: any
+  }>>;
+
+  /**The cache will be updated automatically*/
+  cache: Collection<string, any>;
+  valueLoggingMaxJSONLength: number;
+
+  /**
+   * This class is mostly the same as the DB class but doesn't have a cache so some functions return promises.
+   * @param dbConnectionString MongoDB connection string
+   * @param valueLoggingMaxJSONLength default:20, false to disable value logging
+   */
+  constructor(dbConnectionString: string, collection?: string, valueLoggingMaxJSONLength?: number | false): DB;
+
+  saveLog(msg: string, value: any): NoCacheDB;
+
+  /**@returns `undefined` if no `db` is given */
+  get(db: string, key?: string): Promise<any>;
+  
+  /**@param overwrite overwrite existing collection, default: `false`*/
+  set(db: string, value: any, overwrite?: boolean): Promise<any>;
+  update(db: string, key: string, value: any): Promise<any>;
+  push(db: string, key: string, ...value: any[]): Promise<any[]>;
+  pushToSet(db: string, key: string, ...value: any[]): Promise<any[]>;
+  
+  /**
+   * @param key if not provided, the whole `db` gets deleted
+   * @returns `true` if the element existed
    */
   delete(db: string, key?: string): Promise<boolean>;
 }
