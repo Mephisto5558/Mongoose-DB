8101577dea63da5dc83f1d58e0da86383bd3b4da
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-24 13:27:42 +0100

    added `this` type everywhere

--- index.d.ts
+++ index.d.ts
@@ -17,44 +17,58 @@ declare class NoCacheDB {
    * @param dbConnectionString MongoDB connection string
    * @param valueLoggingMaxJSONLength default:`20`, `false` to disable value logging
    * @param debugLoggingFunction default: `console.debug`*/
-  init(dbConnectionString: string, collection?: string, valueLoggingMaxJSONLength?: number | false, debugLoggingFunction?: (...str: unknown[]) => unknown): Promise<this>;
+  init(this: NoCacheDB, dbConnectionString: string, collection?: string, valueLoggingMaxJSONLength?: number | false, debugLoggingFunction?: (...str: unknown[]) => unknown): Promise<this>;
 
-  saveLog(msg: string, value: unknown): this;
-  reduce(): Promise<{ key: string; value: unknown }[]>;
+  saveLog(this: NoCacheDB, msg: string, value: unknown): this;
+  reduce(this: NoCacheDB): Promise<{ key: string; value: unknown }[]>;
 
-  get(): Promise<undefined>;
-  get(db: string, key?: string): Promise<unknown>;
+  get(this: NoCacheDB): Promise<undefined>;
+  get(this: NoCacheDB, db: string, key?: string): Promise<unknown>;
 
   /** @param overwrite overwrite existing collection, default: `false`*/
-  set(db: string, value: unknown, overwrite?: boolean): Promise<unknown>;
-  update(db: string, key: string, value: unknown): Promise<unknown>;
-  push(db: string, key: string, ...value: unknown[]): Promise<unknown>;
-  pushToSet(db: string, key: string, ...value: unknown[]): Promise<unknown>;
+  set(this: NoCacheDB, db: string, value: unknown, overwrite?: boolean): Promise<unknown>;
+  update(this: NoCacheDB, db: string, key: string, value: unknown): Promise<unknown>;
+  push(this: NoCacheDB, db: string, key: string, ...value: unknown[]): Promise<unknown>;
+  pushToSet(this: NoCacheDB, db: string, key: string, ...value: unknown[]): Promise<unknown>;
 
   /**
    * @param key if not provided, the whole `db` gets deleted
    * @returns `true` if the element existed*/
-  delete(db: string, key?: string): Promise<boolean>;
+  delete(this: NoCacheDB, db: string, key?: string): Promise<boolean>;
 }
 
 declare class DB extends NoCacheDB {
   /** The cache will be updated automatically*/
   cache: Collection<string, unknown>;
 
-  fetchAll(): Promise<this>;
-  fetch(db: string): Promise<unknown>;
+  fetchAll(this: DB): Promise<this>;
+  fetch(this: DB, db: string): Promise<unknown>;
 
   // @ts-expect-error Promiseless method
-  reduce(): { key: string; value: unknown }[];
+  reduce(this: DB): { key: string; value: unknown }[];
 
   // @ts-expect-error Promiseless method
-  get(): undefined;
+  get(this: DB): undefined;
 
   // @ts-expect-error Promiseless method
-  get(db: string, key?: string): unknown;
+  get(this: DB, db: string, key?: string): unknown;
+
+  /** @param overwrite overwrite existing collection, default: `false`*/
+  // @ts-expect-error overwriting `this`
+  set(this: DB, db: string, value: unknown, overwrite?: boolean): Promise<unknown>;
+
+  // @ts-expect-error overwriting `this`
+  update(this: DB, db: string, key: string, value: unknown): Promise<unknown>;
+
+  // @ts-expect-error overwriting `this`
+  push(this: DB, db: string, key: string, ...value: unknown[]): Promise<unknown>;
+
+  // @ts-expect-error overwriting `this`
+  pushToSet(this: DB, db: string, key: string, ...value: unknown[]): Promise<unknown>;
 
   /**
    * @inheritdoc
    * @returns `true` if the element existed or the key param is provied and `false` if the element did not exist*/
-  delete(db: string, key?: string): Promise<boolean>;
+  // @ts-expect-error overwriting `this`
+  delete(this: DB, db: string, key?: string): Promise<boolean>;
 }
