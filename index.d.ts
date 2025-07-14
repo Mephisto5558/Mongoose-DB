ae83b4d176bf2445e74aacc0034bb3b89a0ae3ef
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-07-14 14:01:32 +0200

    made my own types for flattening

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ import type { Document, Model, Types } from 'mongoose';
 export { DB, NoCacheDB };
 export default DB;
 
-declare class NoCacheDB<Database extends Record<string, unknown>, FlattenedDatabase = { [DB in keyof Database]: FlattenObject<Database[DB]>; }> {
+declare class NoCacheDB<Database extends Record<string, unknown>> {
   schema: Model<Document<unknown, Record<string, never>, {
     _id: Types.ObjectId;
     key: string;
@@ -24,110 +24,79 @@ declare class NoCacheDB<Database extends Record<string, unknown>, FlattenedDatab
 
   get(): Promise<undefined>;
   get<DBK extends keyof Database>(db: DBK): Promise<Database[DBK]>;
+  get<DBK extends keyof Database, K extends string>(db: DBK, key: K): Promise<GetValueByKey<Database[DBK], K> | undefined>;
+  get(db: unknown, key?: unknown): Promise<unknown>; // fallback
   get<DBK extends keyof Database, K extends keyof FlattenedDatabase[DBK]>(db: DBK, key: K): Promise<GetResult<Database, FlattenedDatabase, DBK, K>>;
 
   /** @param overwrite overwrite existing collection, default: `false` */
-  set<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK]>(db: DBK, value: FDB[keyof FDB], overwrite?: boolean): ModifyResult<Database, DBK>;
-  update<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(db: DBK, key: K, value: FDB[K]): ModifyResult<Database, DBK>;
-  push<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(db: DBK, key: K, ...value: FDB[K][]): ModifyResult<Database, DBK>;
-  pushToSet<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(db: DBK, key: K, ...value: FDB[K][]): ModifyResult<Database, DBK>;
+  set<DBK extends keyof Database>(db: DBK, value: Partial<Database[DBK]>, overwrite?: boolean): ModifyResult<Database, DBK>;
+  update<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(db: DBK, key: K, value: GetValueByKey<Database[DBK], K>): ModifyResult<Database, DBK>;
 
-  delete(): Promise<false>;
-  delete(db: keyof Database): Promise<true>;
+  push<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(db: DBK, key: K, ...value: (GetValueByKey<Database[DBK], K> extends (infer E)[] ? E : never)[]): ModifyResult<Database, DBK>;
+  pushToSet<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(
+    db: DBK, key: K, ...value: (GetValueByKey<Database[DBK], K> extends (infer E)[] ? E : never)[]
+  ): ModifyResult<Database, DBK>;
+
 
   /**
    * @param key **if not provided, the whole `db` gets deleted**
    * @returns `true` if the element existed */
-  delete<DBK extends keyof Database>(db: DBK, key: keyof FlattenedDatabase[DBK]): Promise<boolean>;
+  delete(): Promise<false>;
+  delete(db: keyof Database): Promise<true>;
+  delete<DBK extends keyof Database>(db: DBK, key: SettingsPaths<Database[DBK]>): Promise<boolean>;
 
   valueOf(): string;
 }
 
-declare class DB<Database extends Record<string, unknown>, FlattenedDatabase = { [DB in keyof Database]: FlattenObject<Database[DB]>; }> extends NoCacheDB<Database, FlattenedDatabase> {
-  /** The cache will be updated automatically */
+declare class DB<Database extends Record<string, unknown>> extends NoCacheDB<Database> {
   cache: Collection<keyof Database, Database[keyof Database]>;
 
   fetchAll(this: DB): Promise<this>;
   fetch<DBK extends keyof Database>(this: DB, db: DBK): Promise<Database[DBK]>;
 
-  reduce(this: DB): Awaited<ReturnType<NoCacheDB<Database, FlattenedDatabase>['reduce']>>;
+  reduce(this: DB): Awaited<ReturnType<NoCacheDB<Database>['reduce']>>;
 
   get(): undefined;
   get<DBK extends keyof Database>(this: DB, db: DBK): Database[DBK];
-  get<DBK extends keyof Database, K extends keyof FlattenedDatabase[DBK]>(this: DB, db: DBK, key: K): GetResult<Database, FlattenedDatabase, DBK, K>;
+  get<DBK extends keyof Database, K extends string>(this: DB, db: DBK, key: K): GetValueByKey<Database[DBK], K> | undefined;
+  get(db: unknown, key?: unknown): unknown; // fallback
 
-  /** @param overwrite overwrite existing collection, default: `false` */
-  set<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK]>(this: DB, db: DBK, value: FDB[keyof FDB], overwrite?: boolean): ModifyResult<Database, DBK>;
+  set<DBK extends keyof Database>(this: DB, db: DBK, value: Partial<Database[DBK]>, overwrite?: boolean): ModifyResult<Database, DBK>;
+  update<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(this: DB, db: DBK, key: K, value: GetValueByKey<Database[DBK], K>): ModifyResult<Database, DBK>;
 
-  update<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(this: DB, db: DBK, key: K, value: FDB[K]): ModifyResult<Database, DBK>;
-  push<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(this: DB, db: DBK, key: K, ...value: FDB[K][]): ModifyResult<Database, DBK>;
-  pushToSet<DBK extends keyof Database, FDB extends FlattenedDatabase[DBK], K extends keyof FDB>(this: DB, db: DBK, key: K, ...value: FDB[K][]): ModifyResult<Database, DBK>;
+  push<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(
+    this: DB, db: DBK, key: K, ...value: (GetValueByKey<Database[DBK], K> extends (infer E)[] ? E : never)[]
+  ): ModifyResult<Database, DBK>;
+  pushToSet<DBK extends keyof Database, K extends SettingsPaths<Database[DBK]>>(
+    this: DB, db: DBK, key: K, ...value: (GetValueByKey<Database[DBK], K> extends (infer E)[] ? E : never)[]
+  ): ModifyResult<Database, DBK>;
 
   delete(): Promise<false>;
   delete(db: keyof Database): Promise<true>;
-
-  /**
-   * @inheritdoc
-   * @returns `true` if the element existed or the key param is provied and `false` if the element did not exist */
-  delete<DBK extends keyof Database>(this: DB, db: DBK, key?: keyof FlattenedDatabase[DBK]): Promise<boolean>;
+  delete<DBK extends keyof Database>(this: DB, db: DBK, key?: SettingsPaths<Database[DBK]>): Promise<boolean>;
 }
-
-type GetResult<Database, FlattenedDatabase, DBK extends keyof Database, K extends keyof FlattenedDatabase[DBK]> = (
- string extends keyof Database[DBK] ? FlattenedDatabase[DBK][K] | undefined : FlattenedDatabase[DBK][K]
-);
 
 type ModifyResult<Database, DBK extends keyof Database> = Promise<Database[DBK]> & {};
 
+export type GetValueByKey<T, K extends string>
+  = K extends `${infer Head}.${infer Tail}`
+    ? string extends keyof T
+      ? GetValueByKey<T[keyof T], Tail>
+      : Head extends keyof T
+        ? GetValueByKey<T[Head], Tail>
+        : undefined
+    : string extends keyof T
+      ? T[keyof T]
+      : K extends keyof T
+        ? T[K]
+        : undefined;
 
-// https://github.com/blazejkustra/dynamode/blob/ab81ad1f412b928ffaad3162654c96fb026751c4/lib/utils/types.ts#L10-L67
+type Primitive = string | number | boolean | bigint | Date | Set<unknown> | Map<unknown, unknown> | undefined | null | unknown[];
 
-// Flatten entity
-export type FlattenObject<TValue> = CollapseEntries<CreateObjectEntries<TValue, TValue>>;
+type Paths<T> = T extends Primitive
+  ? never
+  : {
+    [K in keyof T & string]: T[K] extends Primitive ? K : K | `${K}.${Paths<T[K]>}`
+  }[keyof T & string];
 
-type Entry = { key: string; value: unknown };
-type EmptyEntry<TValue> = { key: ''; value: TValue };
-type ExcludedTypes = Date | Set<unknown> | Map<unknown, unknown> | Uint8Array;
-type ArrayEncoder = `[${bigint}]`;
-
-type EscapeArrayKey<TKey extends string> = TKey extends `${infer TKeyBefore}.${ArrayEncoder}${infer TKeyAfter}`
-  ? EscapeArrayKey<`${TKeyBefore}${ArrayEncoder}${TKeyAfter}`>
-  : TKey;
-
-// Transforms entries to one flattened type
-type CollapseEntries<TEntry extends Entry> = {
-  [E in TEntry as EscapeArrayKey<E['key']>]: E['value'];
-};
-
-// Transforms array type to object
-type CreateArrayEntry<TValue, TValueInitial> = OmitItself<
-  TValue extends unknown[] ? Record<ArrayEncoder, TValue[number]> : TValue,
-  TValueInitial
->;
-
-// Omit the type that references itself
-type OmitItself<TValue, TValueInitial> = TValue extends TValueInitial
-  ? EmptyEntry<TValue>
-  : OmitExcludedTypes<TValue, TValueInitial>;
-
-// Omit the type that is listed in ExcludedTypes union
-type OmitExcludedTypes<TValue, TValueInitial> = TValue extends ExcludedTypes
-  ? EmptyEntry<TValue>
-  : CreateObjectEntries<TValue, TValueInitial>;
-
-type CreateObjectEntries<TValue, TValueInitial> = TValue extends object ? {
-
-  // Checks that Key is of type string
-  [TKey in keyof TValue]-?: TKey extends string
-
-    // Nested key can be an object, run recursively to the bottom
-    ? CreateArrayEntry<TValue[TKey], TValueInitial> extends infer TNestedValue
-      ? TNestedValue extends Entry
-        ? TNestedValue['key'] extends ''
-          ? { key: TKey; value: TNestedValue['value'] }
-          : | { key: `${TKey}.${TNestedValue['key']}`; value: TNestedValue['value'] }
-            | { key: TKey; value: TValue[TKey] }
-        : never
-      : never
-    : never;
-}[keyof TValue] // Builds entry for each key
-  : EmptyEntry<TValue>;
+export type SettingsPaths<T> = string extends keyof T ? Paths<T[keyof T]> : Paths<T>;
